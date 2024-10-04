4ef4dcae7c3cf67da08f1fd138a8ec133edd6787
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-10-04 15:28:36 -0300

    Add Redux slices for cart and UI management and configure store with reducers

---
+++ src/store/cart-slice.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { items: [], totalQuantity: 0 };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          name: newItem.name,
+        });
+      } else {
+        existingItem.quantity++;
+      }
+    },
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem) {
+        state.totalQuantity--;
+        existingItem.quantity--;
+      }
+    },
+    deleteItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+
+      if (existingItem) {
+        state.totalQuantity -= existingItem.quantity;
+        state.items = state.items.filter((item) => item.id !== id);
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
