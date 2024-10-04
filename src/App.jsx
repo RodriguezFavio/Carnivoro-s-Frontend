5030ff843895214f0a4cf3fbab490aaaa804c4d5
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-10-04 15:49:35 -0300

    Added React Router and React Query providers to App component, and wrapped App with Redux Provider in main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import router from './router/router';
+import { queryClient } from './services/meals';
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
